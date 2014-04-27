53b00dbd94c92402b1e613b613abfca33effc049
Artem Antonov <diway@mail.ru>
2014-04-27 21:08:02 +0400

    fix 'binary search' problem solution

--- problems/bins.py
+++ problems/bins.py
@@ -3,10 +3,9 @@
 def bin_search_range(arr, search_value, min_index, max_index):
     if (max_index < min_index or min_index < 0 or max_index >= len(arr)):
         return -1
-    if (max_index == min_index):
-        if arr[min_index] == search_value: return min_index
-        else: return -1
     middle_index = (min_index + max_index) >> 1
+    if (max_index == min_index):
+        return min_index if arr[min_index] == search_value else -1
     if (arr[middle_index] >= search_value):
         return bin_search_range(arr, search_value, min_index, middle_index)
     elif (arr[middle_index] < search_value):
@@ -17,20 +16,21 @@ def bin_search(arr, search_value):
     return bin_search_range(arr, search_value, 0, len(arr)-1)
 
 # Test
-# arr = [10, 20, 30, 40, 50]
-# for search_value in [40, 10, 35, 15, 40, 20]:
-#     print bin_search(arr, search_value)
+arr = [10, 20, 30, 40, 50]
+for search_value in [40, 10, 35, 15, 40, 20]:
+    print bin_search(arr, search_value)
 
-input = open('/home/diway/Desktop/rosalind_bins.txt', 'r')
+input = open('/home/diway/Desktop/in.txt', 'r')
 n = input.readline().strip()
 m = input.readline().strip()
 arr = input.readline().split()
+arr = map(lambda str: int(str), arr)
 search_values = input.readline().split()
 input.close()
 
-output = open('/home/diway/Desktop/rosalind_bins_out.txt', 'w')
+output = open('/home/diway/Desktop/out.txt', 'w')
 for search_value in search_values:
-    index = bin_search(arr, search_value)
+    index = bin_search(arr, int(search_value))
     if (index >= 0):
         index += 1
     output.write(str(index) + ' ') 
